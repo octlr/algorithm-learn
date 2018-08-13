9ffb86b34d5d85a5194ae5b0442c67fcf83bc0e9
octlr <3880428a>
2018-08-13 20:22:16 +0800

    61,92

--- include/leetcode.h
+++ include/leetcode.h
@@ -75,4 +75,53 @@ void testRemoveNthFromEnd();
  */
 struct ListNode *addTwoNumbers(struct ListNode *l1, struct ListNode *l2);
 void testAddTwoNumbers();
+
+/**
+ * 给定一个链表，旋转链表，将链表每个节点向右移动 k 个位置，其中 k 是非负数。
+ *
+ *输入: 1->2->3->4->5->NULL, k = 2
+输出: 4->5->1->2->3->NULL
+解释:
+向右旋转 1 步: 5->1->2->3->4->NULL
+向右旋转 2 步: 4->5->1->2->3->NULL
+ *
+ *
+ *输入: 0->1->2->NULL, k = 4
+输出: 2->0->1->NULL
+解释:
+向右旋转 1 步: 2->0->1->NULL
+向右旋转 2 步: 1->2->0->NULL
+向右旋转 3 步: 0->1->2->NULL
+向右旋转 4 步: 2->0->1->NULL
+ *
+ *
+ *
+ * @param head
+ * @param k
+ * @return
+ */
+struct ListNode* rotateRight(struct ListNode* head, int k);
+void testRotateRight();
+
+
+//
+// Created by Administrator on 2018/8/13.
+//反转从位置 m 到 n 的链表。请使用一趟扫描完成反转。
+//1 ≤ m ≤ n ≤ 链表长度。
+//
+//输入: 1->2->3->4->5->NULL, m = 2, n = 4
+//输出: 1->4->3->2->5->NULL
+//
+
+struct ListNode* reverseBetween(struct ListNode* head, int m, int n);
+void testReverseBetween();
+
+
+
+
+
+
 #endif //ALGORITHM_LEARN_LEETCODE_H
+
+
+
